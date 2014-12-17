ea2a4d426c322f6327881d0ac7b11f307df7e748
Dieter Theiler <dtheiler@tugraz.at>
2014-12-17 14:20:26 +0100

    LL-923

--- SSSClientInterfaceREST/SSConns.js
+++ SSSClientInterfaceREST/SSConns.js
@@ -2150,8 +2150,8 @@ includeOwn){
  * @param {Integer} pageNumber number of the page to be requested from a previous search result
  * @param {Integer} minRating minimum overall star rating the entity must have to be returned
  * @param {Integer} maxRating maximum overall star rating the entity must have to be returned
- * @param {Boolean} localSearchOp how results will be comined for query parameter separately (i.e. or | and; e.g. and: results have to match for all tags given in tagsToSearchFor)
- * @param {Boolean} globalSearchOp how results will be comined overall (i.e. or | and; e.g. and: results have to match all given tags labels) 
+ * @param {Boolean} localSearchOp how results will be combined for query parameters separately (i.e. or | and; e.g. and: results have to match for all tags given in tagsToSearchFor)
+ * @param {Boolean} globalSearchOp how results will be combined overall (i.e. or | and; e.g. and: results have to match all given tags and labels) 
  * @return {SSSearchRet} <br>
  * {SSEntity Array} entities found entities with additional information
  */
